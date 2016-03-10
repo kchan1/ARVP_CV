87d98140e4497e91e76de01aab4ae51718a1de9f
Edwin <edwintan.mail@gmail.com>
2016-03-10 18:58:26 -0500

    Update interoperability.py
    Added telemetry upload test

--- Interoperability/interoperability.py
+++ Interoperability/interoperability.py
@@ -8,6 +8,7 @@ from StringIO import StringIO
 interop_api_url = 'http://155.246.210.164/api/'
 
 # Send POST login session cookie
+
 c = pycurl.Curl()
 
 c.setopt(pycurl.URL, interop_api_url+'login')
@@ -16,6 +17,7 @@ c.setopt(pycurl.COOKIEFILE, 'sessionid.txt')
 c.perform()
 
 # Initial server_info GET request
+
 c = pycurl.Curl()
 
 buffer = StringIO()
@@ -28,6 +30,7 @@ print("\n\n" + "====Server Info====")
 print(buffer.getvalue())
 
 # GET obstacles request
+
 c = pycurl.Curl()
 
 buffer = StringIO()
@@ -43,6 +46,7 @@ print json.dumps(obstacles_json, sort_keys=True,indent=4, separators=(',', ': ')
 #print buffer.getvalue()
 
 # Send POST target 
+
 test_target_data_string = { \
     "type": "standard", \
     "latitude": 38.1478, \
@@ -77,3 +81,11 @@ obstacles_json = json.loads(buffer.getvalue())
 
 print("\n\n" + "====Targets====")
 print json.dumps(obstacles_json, sort_keys=True,indent=4, separators=(',', ': '))
+
+# Send telemetry to server
+c = pycurl.Curl()
+
+c.setopt(pycurl.URL, interop_api_url+'telemetry')
+c.setopt(pycurl.POSTFIELDS, 'latitude=38.149&longitude=-76.432&altitude_msl=100&uas_heading=90')
+c.setopt(pycurl.COOKIEFILE, 'sessionid.txt')
+c.perform()
