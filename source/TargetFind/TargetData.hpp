62a071770777dfda6b52f994a8787a2dbd5b618e
KennithChan <kchan1@stevens.edu>
2016-03-20 12:12:43 -0400

    Minimal progress on actual TargetFind, commiting to bring current progess in

---
+++ source/TargetFind/TargetData.hpp
@@ -0,0 +1,13 @@
+#ifndef _TARGETDATA_HPP_
+#define _TARGETDATA_HPP_
+class TargetData
+{
+public:
+  int width,height;
+  float centroid_x;
+  float centroid_y;
+  float circum_radius;
+  //Telem* telem;
+private:
+};
+#endif
