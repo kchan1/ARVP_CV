d202e3704b4b8ae2c9474128bfdc5a8c48005f24
KennithChan <kchan1@stevens.edu>
2016-04-22 17:08:26 -0400

    Added Untested but Compiling code for blob consolidation

--- source/TargetFind/BlobDetect.hpp
+++ source/TargetFind/BlobDetect.hpp
@@ -4,6 +4,7 @@
 #include "../imgformat.hpp"
 #include "../imgops.hpp"
 #include "../LinkedList.hpp"
+#include "../geometry.hpp"
 #include <gsl/gsl_blas.h>
 #include "TargetData.hpp"
 #include <math.h>
@@ -11,96 +12,161 @@
 #include <cstdlib>
 #include <stdio.h>
 #include <string.h>
-//small helper class to stuff into linked lists
-class BlobCandidate
+
+class pointGroup
 {
- public:
-  int scale,ypos,xpos,intensity;
-  BlobCandidate(int scale,int ypos,int xpos,int intensity)
+private:
+  Point2D<double>*centroid;
+  double circum_radius;
+  LinkedList<Point2D<int> >*members;
+public:
+  pointGroup(Point2D<int>*init_pt)
   {
-    this->scale = scale;
-    this->ypos = ypos;
-    this->xpos = xpos;
-    this->intensity = intensity;
+    this->members = new LinkedList<Point2D<int> >();
+    members->push(init_pt);
+    this->circum_radius = 1.0;
+  }
+  ~pointGroup()
+  {
+    delete this->centroid;
+    delete this->members;
+  }
+  double getRadius()
+  {
+    return this->circum_radius;
+  }
+  Point2D<double>*getCentroid()
+  {
+    return this->centroid;
+  }
+  void addPoints(LinkedList<Point2D<int> >*pts)
+  {
+    //continuously recalculate the centroid
+    while(pts->hasNext())
+    {
+      Point2D<int>*pt=pts->pop();
+      members->push(pt);
+      //update the centroid
+      double xcent = this->centroid->xpos;
+      double ycent = this->centroid->ypos;
+      xcent = xcent*(members->getSize()-1)/members->getSize() + pt->xpos/members->getSize();
+      ycent = ycent*(members->getSize()-1)/members->getSize() + pt->ypos/members->getSize();
+      this->centroid->xpos = xcent;
+      this->centroid->ypos = ycent;
+    }
+    
+    //update the radius
+    double maxsqrdist=1;
+    for(int i=0;i<members->getSize();i++)
+    {
+      double sqrdist=pow(this->centroid->xpos-members->get(i)->xpos,2)+pow(this->centroid->ypos-members->get(i)->ypos,2);
+      if(sqrdist>maxsqrdist)
+	maxsqrdist=sqrdist;
+    }
+    this->circum_radius = sqrt(maxsqrdist);
+  }
+  friend void absorbGroup(pointGroup*group1,pointGroup*group2)
+  {
+    group1->addPoints(group2->members);
+    group2->circum_radius=0;
   }
 };
 
-//detects targets at a specified scale
-LinkedList<BlobCandidate>*detectScale(ARVP_Image*src_img,int init_scale,int max_scale, int threshold)
+bool canGrab(pointGroup*group1,pointGroup*group2)
 {
-  printf("detectScale();\n");
-  int scale,j,i;
-  ARVP_Image*gradient_img = new ARVP_Image(src_img->height,src_img->width);
-  ARVP_Image*scale_space_img = new ARVP_Image(src_img->height,src_img->width);
-  //this is what is returned
-  LinkedList<BlobCandidate>*candidate_list = new LinkedList<BlobCandidate>();
-  gsl_matrix*filter;
-
-  //perform gradient, the blob is found as a low-gradient area
-  printf("Performing gradient operation\n");
-  gradient_RGB(src_img,gradient_img,true);
-  ///perform canny edge detection, blob is found as edge-less region
-  //printf("Beginning Canny Edge Detection\n");
-  //cannyEdgeDetection(src_img,gradient_img,1,10,2);
-  //gradient_img->debugPrint();
-  //printf("Beginning the scales\n");
-  for(scale = init_scale;scale<=max_scale;scale++)
+  Point2D<double>*cent1 = group1->getCentroid();
+  Point2D<double>*cent2 = group2->getCentroid();
+  double sqrdist = pow(cent1->xpos-cent2->xpos,2)+pow(cent1->ypos-cent2->ypos,2);
+  if(sqrdist<=pow(group1->getRadius()*1.5,2))
   {
-    //perform gaussian blur for scale space
-    int blur_size = (int)ceil(sqrt(scale)*6);
-    if(scale>0)
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+void growGroups(LinkedList<pointGroup>*groups)
+{
+  LinkedList<pointGroup>*ready=groups;
+  LinkedList<pointGroup>*done=new LinkedList<pointGroup>;
+  while(ready->hasNext())
+  {
+    pointGroup*group = ready->pop();
+    bool hasRegrouped = false;
+    for(int i=0;i<ready->getSize();i++)
     {
-      filter = gsl_matrix_calloc(blur_size,blur_size);
-      gaussian(filter,scale);
-    }
-    else
-    {
-      filter = gsl_matrix_calloc(1,1);
-      gsl_matrix_set(filter,0,0,1);
-    }
-    printf("Doing Gaussian\n");
-    convolution_RGB(gradient_img,scale_space_img,filter,blur_size/2,blur_size/2);
-    gsl_matrix_free(filter);
-    //perform laplacian operator
-    //strong positive reponse for blobs of size sqrt(2*scale)
-    //filter = gsl_matrix_calloc(blur_size,blur_size);
-    //laplace(filter);
-    int smoothsize = 9;
-    filter = gsl_matrix_calloc(smoothsize,smoothsize);
-    for(j=0;j<smoothsize;j++)
-      for(i=0;i<smoothsize;i++)
-	gsl_matrix_set(filter,j,i,1.0/pow(smoothsize,2));
-    convolution_RGB(scale_space_img,scale_space_img,filter,smoothsize/2,smoothsize/2); 
-    /*
-    for(j=0;j<(int)filter->size1;j++)
-    {
-      for(i=0;i<(int)filter->size2;i++)
-	printf("FILTER %i,%i = %f, ",j,i,gsl_matrix_get(filter,j,i));
-      printf("\n");
-    }
-    */
-    //printf("Filter");
-    //printf("Doing Laplacian\n");
-    //convolution_RGB(scale_space_img,scale_space_img,filter,1,1);
-    //gsl_matrix_free(filter);
-    //gather candidate results with the scale,position,and intensity
-    printf("Getting Candidates\n");
-    for(j=0;j<(int)scale_space_img->height;j++)
-      for(i=0;i<(int)scale_space_img->width;i++)
+      if(canGrab(group,ready->get(i)))
       {
-	//if the pixel is above the threshold, add it to the candidate list
-	//later add the maxima condition maybe
-	int intensity = (int)scale_space_img->get_ch(0,j,i);
-	//if(j==i)
-	//  printf("INTENSITY %i,%i = %i\n",j,i,intensity);
-	if(intensity<=threshold)
-	{
-	  //printf("Got one!\n");
-	  //candidate_list->push(new BlobCandidate(init_scale,j,i,intensity));
-	  scale_space_img->set_ch(1,j,i,255);
-	}
+	hasRegrouped = true;
+	pointGroup*food = ready->remove(i);
+	absorbGroup(group,food);
+	delete food;
+	ready->push(group);
+	break;
       }
-    
+    }
+    if(hasRegrouped)
+      continue;
+    for(int i=0;i<done->getSize();i++)
+    {
+      if(canGrab(group,done->get(i)))
+      {
+	hasRegrouped = true;
+	pointGroup*food = done->remove(i);
+	absorbGroup(group,food);
+	delete food;
+	ready->push(group);
+	break;
+      }
+    }
+    if(!hasRegrouped)
+    {
+      done->push(group);
+    }
+  }
+}
+
+//detects targets at a specified scale
+void detectLowGradeScale(ARVP_Image*scale_space_img,int scale,int threshold,LinkedList<Point2D<int> >*candidate_list,bool debug=false)
+{
+  gsl_matrix*filter;
+  int j,i;
+  //smooth the image
+  int smoothsize = (1+scale-(scale)%2)*3;
+  filter = gsl_matrix_calloc(smoothsize,smoothsize);
+  for(j=0;j<smoothsize;j++)
+    for(i=0;i<smoothsize;i++)
+      gsl_matrix_set(filter,j,i,1.0/pow(smoothsize,2));
+  convolution_RGB(scale_space_img,scale_space_img,filter,smoothsize/2,smoothsize/2); 
+  //perform laplacian operator
+  //strong positive reponse for blobs of size sqrt(2*scale)
+  //filter = gsl_matrix_calloc(3,3);
+  //laplace(filter);
+  //printf("Doing Laplacian\n");
+  //convolution_RGB(scale_space_img,scale_space_img,filter,1,1);
+  //gsl_matrix_free(filter);
+
+  //gather candidate results with the scale,position,and intensity
+  printf("Getting Candidates\n");
+  for(j=0;j<(int)scale_space_img->height;j++)
+    for(i=0;i<(int)scale_space_img->width;i++)
+    {
+      //if the pixel is above the threshold, add it to the candidate list
+      //later add the maxima/minima condition maybe
+      //yes this is arbitrarily accessing the red channel
+      int intensity = (int)scale_space_img->get_ch(0,j,i);
+      if(intensity<=threshold)
+      {
+	//printf("Got one!\n");
+	candidate_list->push(new Point2D<int>(i,j));
+	if(debug)
+	  scale_space_img->set_ch(1,j,i,255);
+      }
+    }
+  if(debug)
+  {
     printf("Finished scale %i\n",scale);
     char scale_str[2];
     char filename_head[] = "img_";
@@ -109,7 +175,58 @@ LinkedList<BlobCandidate>*detectScale(ARVP_Image*src_img,int init_scale,int max_
     const char* filename = strcat(filename_head,strcat(scale_str,filename_tail));
     saveARVP_Image(scale_space_img,filename);
   }
-  printf("Finished detecting, now deleing\n");
+}
+
+
+
+//detects targets at a specified scale as a grouping of edges
+LinkedList<Point2D<int> >*cannyGroupDetect(ARVP_Image*src_img,int init_scale,int max_scale, int threshold,bool debug = false)
+{
+  ARVP_Image*edge_img = new ARVP_Image(src_img->height,src_img->width);
+  //this is what is returned
+  LinkedList<Point2D<int> >*candidate_list = new LinkedList<Point2D<int> >();
+  //perform canny edge detection, blob is found as hi-edge region
+  printf("Beginning Canny Edge Detection\n");
+  cannyEdgeDetection(src_img,edge_img,init_scale,30,10);
+  //put edges in groups
+  //grow the groups
+  printf("Finished detecting, now deleting\n");
+  delete edge_img;
+  return candidate_list;
+}
+
+//detects targets at a specified scale as a low-gradient section
+LinkedList<Point2D<int> >*gradeSmoothDetect(ARVP_Image*src_img,int init_scale,int max_scale, int threshold,bool debug = false)
+{
+  int scale;
+  ARVP_Image*gradient_img = new ARVP_Image(src_img->height,src_img->width);
+  ARVP_Image*scale_space_img = new ARVP_Image(src_img->height,src_img->width);
+  //this is what is returned
+  LinkedList<Point2D<int> >*candidate_list = new LinkedList<Point2D<int> >();
+  gsl_matrix*filter;
+
+  //perform gradient, the blob is found as a low-gradient area
+  printf("Performing gradient operation\n");
+  gradient_RGB(src_img,gradient_img,true);
+  //the image is evaluated at scales
+  //printf("Beginning the scales\n");
+  //for(scale = init_scale;scale<=max_scale;scale++)
+  for(scale = init_scale;scale<=init_scale;scale++) //ignore max_scale for now
+  {
+    //perform gaussian blur for scale space
+    int blur_size = (int)ceil(sqrt(scale)*6);
+    if(scale>0)
+    {
+      filter = gsl_matrix_calloc(blur_size,blur_size);
+      gaussian(filter,scale);
+      printf("Doing Gaussian\n");
+      convolution_RGB(gradient_img,scale_space_img,filter,blur_size/2,blur_size/2);
+      gsl_matrix_free(filter);
+    }
+    //pass the scale space image to the detection
+    detectLowGradeScale(gradient_img,scale,threshold,candidate_list,debug);
+  }
+  printf("Finished detecting, now deleting\n");
   delete gradient_img;
   delete scale_space_img;
   return candidate_list;
--- source/TargetFindTest.cpp
+++ source/TargetFindTest.cpp
@@ -1,7 +1,7 @@
-#include "BlobDetect.hpp"
-#include "../ARVP_JPEG.hpp"
-#include "../imgformat.hpp"
-#include "../imgops.hpp"
+#include "TargetFind/BlobDetect.hpp"
+#include "ARVP_JPEG.hpp"
+#include "imgformat.hpp"
+#include "imgops.hpp"
 #include <gsl/gsl_blas.h>
 #include <iostream>
 #include <stdio.h>
@@ -13,24 +13,24 @@ int main(void)
   //const char* outname = "write.jpg";
   std::cout<<"--Testing has begun on opening an image--\n";
   ARVP_Image*test_img_raw = openJPEG(filename);
+  /*
   ARVP_Image_Resize*test_img_scaled = new ARVP_Image_Resize(test_img_raw->height/2,
   							    test_img_raw->width/2,
   							    test_img_raw);
   ARVP_Image*test_img = (ARVP_Image*)test_img_scaled;
-  //ARVP_Image*test_img = test_img_raw;
+  */
+  ARVP_Image*test_img = test_img_raw;
   printf("Opening image %s\n",filename);
   printf("Original Image of size w,h: %i,%i\n",(int)test_img_raw->width,(int)test_img_raw->height);
   printf("Image of size w,h: %i,%i\n",(int)test_img->width,(int)test_img->height);
   printf("Blobbing the crisp image\n");
-  LinkedList<BlobCandidate>* candidate_list = detectScale(test_img,0,4,20);
+  LinkedList<Point2D<int> >* candidate_list = gradeSmoothDetect(test_img,0,4,10,true);
   while (candidate_list->hasNext())
   {
-    BlobCandidate*candidate = candidate_list->pop();
-    printf("Candidate at S:%i, Y:%i, X:%i, I:%i\n",
-	   candidate->scale,
+    Point2D<int>*candidate = candidate_list->pop();
+    printf("Candidate at Y:%i, X:%i\n",
 	   candidate->ypos,
-	   candidate->xpos,
-	   candidate->intensity);
+	   candidate->xpos);
     delete candidate;
   }
   delete candidate_list;
